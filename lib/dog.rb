d913f743bbe2c91dfe368c6be641554f418417aa
Sean Martin <sean@seanmartinstudio.com>
2022-06-04 11:33:17 -0700

    commit tree c6182d6e0c584dcd64f6a5d7aa89d1ad44a00259

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,23 @@
 class Dog
 
+    attr_reader :id, :name, :breed
+
+    def initialize(id: nil, name:, breed:)
+        @id = id
+        @name = name
+        @breed = breed
+    end
+
+    def self.create_table
+        sql = <<-SQL
+        CREATE TABLE IF EXISTS dogs (
+          id INTEGER PRIMARY KEY,
+          name TEXT,
+          album TEXT
+        )
+      SQL
+      DB[:conn].execute(sql)
+    end
+
+
 end
